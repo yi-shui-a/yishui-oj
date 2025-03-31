e5214026b1243cdd3c5a7e89d6548d335a64cc19
yishui <1480685741@qq.com>
2025-03-31 13:09:25 +0800

    add: 代码沙箱，服务器执行程序，返回输出功能。代码沙箱核心流程开发完毕

--- yishui-oj-code-sandbox/src/main/java/com/yishui/yishuiojcodesandbox/JavaNativeCodeSandbox.java
+++ yishui-oj-code-sandbox/src/main/java/com/yishui/yishuiojcodesandbox/JavaNativeCodeSandbox.java
@@ -2,18 +2,25 @@ package com.yishui.yishuiojcodesandbox;
 
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.io.resource.ResourceUtil;
+import cn.hutool.core.util.StrUtil;
 import com.yishui.yishuiojcodesandbox.model.ExecuteCodeRequest;
 import com.yishui.yishuiojcodesandbox.model.ExecuteCodeResponse;
+import com.yishui.yishuiojcodesandbox.model.ExecuteMessage;
+import com.yishui.yishuiojcodesandbox.model.JudgeInfo;
+import com.yishui.yishuiojcodesandbox.utils.ProcessUtils;
 import org.springframework.util.ResourceUtils;
+import org.springframework.util.StopWatch;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class JavaNativeCodeSandbox implements CodeSandbox {
 
@@ -26,6 +33,7 @@ public class JavaNativeCodeSandbox implements CodeSandbox {
         ExecuteCodeRequest executeCodeRequest = new ExecuteCodeRequest();
         executeCodeRequest.setInputList(Arrays.asList("1 2","1 3"));
         String code = ResourceUtil.readStr("testcode/simpleComputeArgs/Main.java", StandardCharsets.UTF_8);
+//        String code = ResourceUtil.readStr("testcode/simpleCompute/Main.java", StandardCharsets.UTF_8);
         executeCodeRequest.setCode(code);
         executeCodeRequest.setLanguage("java");
         ExecuteCodeResponse executeCodeResponse = javaNativeCodeSandbox.executeCode(executeCodeRequest);
@@ -37,12 +45,9 @@ public class JavaNativeCodeSandbox implements CodeSandbox {
         List<String> inputList = executeCodeRequest.getInputList();
         String code = executeCodeRequest.getCode();
         String language = executeCodeRequest.getLanguage();
-//      1.把用户的代码保存为文件
-
-//      3.执行代码，得到输出结果!
-//      4.收集整理输出结果
-//      5.文件清理，释放空间
-//      6.错误处理，提升程序健壮性
+        /**
+         * 1.把用户的代码保存为文件
+         */
         String userDir = System.getProperty("user.dir");
         String globalCodePathName = userDir + File.separator + GLOBAL_CODE_DIR_NAME;
         // 判断全局代码目录是否存在
@@ -56,50 +61,97 @@ public class JavaNativeCodeSandbox implements CodeSandbox {
         File userCodeFile = FileUtil.writeString(code, userCodePath, StandardCharsets.UTF_8);
 
 
-//      2.编译代码，得到 class 文件
+        /**
+         * 2.编译代码，得到 class 文件
+         */
         String compileCmd = String.format("javac -encoding utf-8 %s", userCodeFile.getAbsolutePath());
         try {
-            Process CompileProcess = Runtime.getRuntime().exec(compileCmd);
-            int exitValue = CompileProcess.waitFor();
-            // 正常退出
-            if (exitValue == 0) {
-                System.out.println("编译成功");
-                // 分批获取进程的输出
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(CompileProcess.getInputStream()));
-                StringBuilder compileOutputStringBuilder = new StringBuilder();
-                // 逐行读取
-                String compileOutputLine;
-                while ((compileOutputLine = bufferedReader.readLine()) != null){
-                    compileOutputStringBuilder.append(compileOutputLine);
-                }
-                System.out.println(compileOutputStringBuilder.toString());
-            }else{
-                // 异常退出
-                System.out.println("编译失败，错误码：" + exitValue);
-                // 分批获取进程的输出
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(CompileProcess.getInputStream()));
-                StringBuilder compileOutputStringBuilder = new StringBuilder();
-                // 逐行读取
-                String compileOutputLine;
-                while ((compileOutputLine = bufferedReader.readLine()) != null){
-                    compileOutputStringBuilder.append(compileOutputLine);
-                }
-
-                // 分批获取进程的输出
-                BufferedReader errorBufferedReader = new BufferedReader(new InputStreamReader(CompileProcess.getErrorStream()));
-                StringBuilder errorCompileOutputStringBuilder = new StringBuilder();
-                // 逐行读取
-                String errorCompileOutputLine;
-                while ((errorCompileOutputLine = errorBufferedReader.readLine()) != null){
-                    errorCompileOutputStringBuilder.append(errorCompileOutputLine);
-                }
-                System.out.println(compileOutputStringBuilder.toString());
-                System.out.println(errorCompileOutputStringBuilder.toString());
-            }
-        } catch (IOException | InterruptedException e) {
-            throw new RuntimeException(e);
+            Process compileProcess = Runtime.getRuntime().exec(compileCmd);
+            ExecuteMessage executeMessage = ProcessUtils.runProcessAndGetMessage(compileProcess, "编译");
+            System.out.println(executeMessage);
+        } catch (Exception e) {
+            return getErrorResponse(e);
         }
-        return null;
+
+        /**
+         * 3.执行代码，得到输出结果!
+         */
+        List<ExecuteMessage> executeMessageList = new ArrayList<>();
+        for (String inputArgs : inputList) {
+
+            String runCmd = String.format("java -Dfile.encoding=UTF-8 -cp %s Main %s", userCodeParentPath, inputArgs);
+            try {
+                Process runProcess = Runtime.getRuntime().exec(runCmd);
+                ExecuteMessage executeMessage = ProcessUtils.runProcessAndGetMessage(runProcess, "运行");
+                System.out.println(executeMessage);
+                executeMessageList.add(executeMessage);
+            }catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        /**
+         * 4.收集整理输出结果
+         */
+        ExecuteCodeResponse executeCodeResponse = new ExecuteCodeResponse();
+        List<String> outputList = new ArrayList<>();
+        // 取用时最大值，便于判断是否超时
+        long maxTime = 0;
+        for (ExecuteMessage executeMessage : executeMessageList) {
+            String errorMessage = executeMessage.getErrorMessage();
+            if(StrUtil.isNotBlank(errorMessage)) {
+                executeCodeResponse.setMessage(errorMessage);
+                // 执行中存在错误
+                executeCodeResponse.setStatus(3);
+                break;
+            }
+            outputList.add(executeMessage.getMessage());
+            Long time = executeMessage.getTime();
+            if(time != null){
+                maxTime = Math.max(maxTime, time);
+            }
+        }
+        //正常运行完成
+        if(outputList.size()  == executeMessageList.size()) {
+            executeCodeResponse.setStatus(1);
+        }
+        executeCodeResponse.setOutputList(outputList);
+
+        JudgeInfo judgeInfo = new JudgeInfo();
+//        judgeInfo.setMessage();
+        judgeInfo.setTime(maxTime);
+        // 要借助第三方库来获取内存占用，非常麻烦，此处不做实现
+//        judgeInfo.setMemory();
+        executeCodeResponse.setJudgeInfo(judgeInfo);
+
+        /**
+         * 5.文件清理，释放空间
+         */
+        if(userCodeFile.getParentFile() !=null){
+            boolean del = FileUtil.del(userCodeParentPath);
+            System.out.println("删除" + (del ? "成功" : "失败"));
+        }
+
+        /**
+         * 6.错误处理，提升程序健壮性
+         */
+
+        return executeCodeResponse;
+    }
+
+    /**
+     * 获取错误响应
+     * @param e
+     * @return
+     */
+    private ExecuteCodeResponse getErrorResponse(Throwable e){
+        ExecuteCodeResponse executeCodeResponse = new ExecuteCodeResponse();
+        executeCodeResponse.setOutputList(new ArrayList<>());
+        executeCodeResponse.setMessage(e.getMessage());
+        // 表示代码沙箱错误
+        executeCodeResponse.setStatus(2);
+        executeCodeResponse.setJudgeInfo(new JudgeInfo());
+        return executeCodeResponse;
     }
 
 }
---
+++ yishui-oj-code-sandbox/src/main/java/com/yishui/yishuiojcodesandbox/YishuiOjCodeSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.yishui.yishuiojcodesandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YishuiOjCodeSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YishuiOjCodeSandboxApplication.class, args);
+    }
+
+}
